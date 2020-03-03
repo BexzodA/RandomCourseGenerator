657ec9446f7644c1eb96ee1f683686fa2479b9c8
Bexzod Abduvaliev <bexzodabduvaliev@gmail.com>
2020-03-02 21:39:53 -0500

    removed personal info in project

--- RandomCourseGen/src/RandCourseGenMain.java
+++ RandomCourseGen/src/RandCourseGenMain.java
@@ -1,6 +1,6 @@
 
 public class RandCourseGenMain {
 	public static void main(String[] args) {
-		new RandomCourseGenerator("C:\\Users\\Bexzods\\eclipse-workspace\\HW#2\\src", "students.txt", 99);
+		new RandomCourseGenerator(".\\src", "students.txt", 99);
 	}
 }
